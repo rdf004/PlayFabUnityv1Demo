6ae3b1b8dafa13c4b08f41ae2c5f16ea8f56b7f1
rdf004 <32603533+rdf004@users.noreply.github.com>
2020-06-29 15:38:34 -0700

    Update ButtonHandler.cs

--- ButtonHandler.cs
+++ ButtonHandler.cs
@@ -1,6 +1,9 @@
+using System;
+
 using PlayFab;
 using PlayFab.GroupsModels;
 using PlayFab.ClientModels;
+using PlayFab.DataModels;
 using PlayFab.Json;
 
 using System.Collections;
@@ -14,9 +17,14 @@ public class ButtonHandler : MonoBehaviour
 
     public readonly HashSet<KeyValuePair<string, string>> EntityGroupPairs = new HashSet<KeyValuePair<string, string>>();
     public readonly Dictionary<string, string> GroupNameById = new Dictionary<string, string>();
+
+    public static string player_entityKeyId;
+    public static string player_entityKeyType;
+
+    public static string group_entityKeyId;
+    public static string group_entityKeyType;
     
-    public void clicked()
-    {
+    public void clicked() {
         if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId)){
             /*
             Please change the titleId below to your own titleId from PlayFab Game Manager.
@@ -25,10 +33,216 @@ public class ButtonHandler : MonoBehaviour
             PlayFabSettings.staticSettings.TitleId = "1D8CF";
         }
 
-        var request = new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true};
-        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
+        var request = new LoginWithPlayFabRequest {Username = "lit", TitleId = "1D8CF", Password = "yeetus"};
+        PlayFabClientAPI.LoginWithPlayFab(request, OnLoginSuccess, OnLoginFailure);
 
     }
+
+    private void OnLoginSuccess(LoginResult result) {
+
+        Debug.Log(result.EntityToken.Entity.Id);
+        ButtonHandler.player_entityKeyId = result.EntityToken.Entity.Id;
+        ButtonHandler.player_entityKeyType = result.EntityToken.Entity.Type;
+
+        CreateWishlist();
+
+    }
+
+    public void checkIfWishListExists() {
+
+        PlayFab.GroupsModels.EntityKey entity = new PlayFab.GroupsModels.EntityKey {Id = ButtonHandler.player_entityKeyId, Type = ButtonHandler.player_entityKeyType };
+
+        var request = new ListMembershipRequest { Entity = entity };
+
+        PlayFabGroupsAPI.ListMembership(request, result => {
+
+            Debug.Log(result.Groups[0].GroupName);
+            bool found = false;
+
+            for (int i = 0; i < result.Groups.Count; i++ ) {
+                
+                Debug.Log(result.Groups.Count);
+                Debug.Log(i);
+                string group_name = ButtonHandler.player_entityKeyId + "wishlist";
+                if( result.Groups[i].GroupName.Equals( group_name ) ) {
+
+                    // Found our group!
+                    found = true;
+
+                    PlayFab.DataModels.EntityKey group_ek = new PlayFab.DataModels.EntityKey { Id = result.Groups[i].Group.Id, Type = result.Groups[i].Group.Type };
+
+                    // Assuming that if the group exists, the wishlist exists
+
+                    // Get wishlist object from group entity
+
+                    GetObjectsRequest getObjectsRequest = new GetObjectsRequest { Entity = group_ek };
+
+                    PlayFabDataAPI.GetObjects(getObjectsRequest, objectResult => {
+                        
+                        // JsonObject jsonResult = (JsonObject) objectResult.Objects["wishlist"].DataObject;
+
+                        Debug.Log(objectResult.Objects["wishlist"].DataObject); // This returns either the object or a CSV, can use for durables or consumables
+
+                        string x = (string) objectResult.Objects["wishlist"].DataObject;
+
+                        x += ",";
+                        x += "albatross, dog, mango";
+
+                        UpdateWishlist(group_ek.Id, group_ek.Type, x);
+
+                        Debug.Log(RemoveItemFromCSV(x, "cat"));
+                        
+                    }, error => {
+                        Debug.LogError(error.GenerateErrorReport());
+                    });               
+
+                    // Set object
+
+                    // Case 1: Add item to wishlist
+                    // Case 1a: Success
+                    // Case 1b: Item already on wishlist
+
+                    // SetObjectsRequest setObjReq = new SetObjectsRequest { Entity = group_ek }
+
+                    // Case 2: Remove item from wishlist
+                    // Case 2a: Success
+                    // Case 2b: Item not on wishlist
+
+
+
+                    
+                } 
+            }
+
+            // Group doesn't exist, so we need to create the group and wishlist
+
+            if( found == false ) {
+
+            }
+
+        }, error => {
+            Debug.LogError(error.GenerateErrorReport());
+        });
+
+    }
+
+    private static string RemoveItemFromCSV(string csv, string item_id) {
+        string[] items = csv.Split(',');
+        int ind = Array.IndexOf(items, item_id);
+        List<string> items_list = new List<string>(items);
+
+        if( ind != -1 ) {
+            items_list.RemoveAt(ind);
+        }
+
+        string updated_csv = string.Join(",", items_list);
+
+        return updated_csv;
+
+    }
+
+    private static bool CheckIfItemInWishlist(string csv, string item_id) {
+        string[] items = csv.Split(',');
+        int ind = Array.IndexOf(items, item_id);
+        List<string> items_list = new List<string>(items);
+
+        if( ind != -1 ) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    // private static void addItemtoWishlist(string csv, string item_id) {
+
+    // }
+
+    // private static void removeItemfromWishlist(string csv, string item_id) {
+
+    // }
+
+    private static void UpdateWishlist(string entitykeyId, string entitykeyType, string dataobj) {
+
+        PlayFabCloudScriptAPI.ExecuteEntityCloudScript(new PlayFab.CloudScriptModels.ExecuteEntityCloudScriptRequest() {
+
+            Entity = new PlayFab.CloudScriptModels.EntityKey { Id = entitykeyId, Type = entitykeyType },
+            FunctionName = "addItemtoWishlist",
+            FunctionParameter = new { prop1 = dataobj },
+            GeneratePlayStreamEvent = true
+
+        }, result => {
+            Debug.Log(result.FunctionResult);
+
+        }, error => { Debug.LogError(error.GenerateErrorReport()); });
+
+    }
+
+
+
+    private static void CreateWishlist() {
+
+        PlayFabCloudScriptAPI.ExecuteEntityCloudScript(new PlayFab.CloudScriptModels.ExecuteEntityCloudScriptRequest() {
+
+            Entity = new PlayFab.CloudScriptModels.EntityKey { Id = ButtonHandler.player_entityKeyId, Type = ButtonHandler.player_entityKeyType },
+            FunctionName = "createUserWishList", // Arbitrary function name (must exist in your uploaded cloud.js file)
+            FunctionParameter = new { groupName = "wishlisttestyboi1da612" }, // The parameter provided to your function
+            GeneratePlayStreamEvent = true // Optional - Shows this event in PlayStream
+
+        }, result => {
+
+            Debug.Log(PlayFabSimpleJson.SerializeObject(result.FunctionResult));
+            JsonObject jsonResult = (JsonObject)result.FunctionResult;
+
+            object ek_id;
+            jsonResult.TryGetValue("ek_id", out ek_id); // note how "messageValue" directly corresponds to the JSON values set in CloudScript
+            string ek_id_string = (string)ek_id;
+
+            object ek_type;
+            jsonResult.TryGetValue("ek_type", out ek_type); // note how "messageValue" directly corresponds to the JSON values set in CloudScript
+            string ek_type_string = (string)ek_type;
+
+            AddItemToWishlist(ButtonHandler.player_entityKeyId, ButtonHandler.player_entityKeyType, ek_id_string, ek_type_string, "cat");
+
+        }, error => { Debug.LogError(error.GenerateErrorReport()); });
+
+    }
+
+    private static void AddItemToWishlist(string my_id, string my_type, string ek_id, string ek_type, string dataobj) {
+
+        PlayFabCloudScriptAPI.ExecuteEntityCloudScript(new PlayFab.CloudScriptModels.ExecuteEntityCloudScriptRequest() {
+
+            Entity = new PlayFab.CloudScriptModels.EntityKey {Id = ek_id, Type = ek_type},
+            FunctionName = "addItemtoWishlist",
+            FunctionParameter = new { prop1 = dataobj },
+            GeneratePlayStreamEvent = true
+
+        }, result => {
+            Debug.Log(result.FunctionResult);
+
+        }, error => { Debug.LogError(error.GenerateErrorReport()); });
+
+    }
+
+
+
+    // private static void OnCloudHelloWorld(ExecuteCloudScriptResult result) {
+
+    //     // CloudScript returns arbitrary results, so you have to evaluate them one step and one parameter at a time
+    //     Debug.Log(PlayFabSimpleJson.SerializeObject(result.FunctionResult));
+    //     JsonObject jsonResult = (JsonObject)result.FunctionResult;
+    //     object created;
+    //     jsonResult.TryGetValue("created", out created); // note how "messageValue" directly corresponds to the JSON values set in CloudScript
+    //     Debug.Log((string)created);
+
+    // }
+
+    private static void OnErrorShared(PlayFabError error) {
+
+        Debug.Log(error.GenerateErrorReport());
+
+    }
+
 
     public static PlayFab.GroupsModels.EntityKey EntityKeyMaker(string entityId) {
 
@@ -67,14 +281,6 @@ public class ButtonHandler : MonoBehaviour
         }
     }
 
-    private void OnLoginSuccess(LoginResult result)
-    {
-        Debug.Log("Congratulations, you made your first successful API call!");
-
-        Debug.Log("Yeet");
-        StartCloudHelloWorld(result.EntityToken.Entity.Id, result.EntityToken.Entity.Type);
-        Debug.Log("Yote");
-    }
 
     private void OnLoginFailure(PlayFabError error)
     {
@@ -83,32 +289,7 @@ public class ButtonHandler : MonoBehaviour
         Debug.LogError(error.GenerateErrorReport());
     }
 
-    private static void StartCloudHelloWorld(string my_id, string my_type)
-    {
-        PlayFabCloudScriptAPI.ExecuteEntityCloudScript(new PlayFab.CloudScriptModels.ExecuteEntityCloudScriptRequest()
-        {
-            Entity = new PlayFab.CloudScriptModels.EntityKey {Id = my_id, Type = my_type},
-            FunctionName = "createUserWishList", // Arbitrary function name (must exist in your uploaded cloud.js file)
-            FunctionParameter = new { groupName = "myWishlist2" }, // The parameter provided to your function
-            GeneratePlayStreamEvent = true // Optional - Shows this event in PlayStream
-        }, result => {
-            Debug.Log(result.FunctionResult);
-        }, error => { Debug.LogError(error.GenerateErrorReport()); });
-    }
 
-    private static void OnCloudHelloWorld(ExecuteCloudScriptResult result) {
-        // CloudScript returns arbitrary results, so you have to evaluate them one step and one parameter at a time
-        Debug.Log(PlayFabSimpleJson.SerializeObject(result.FunctionResult));
-        JsonObject jsonResult = (JsonObject)result.FunctionResult;
-        object created;
-        jsonResult.TryGetValue("created", out created); // note how "messageValue" directly corresponds to the JSON values set in CloudScript
-        Debug.Log((string)created);
-    }
-
-    private static void OnErrorShared(PlayFabError error)
-    {
-        Debug.Log(error.GenerateErrorReport());
-    }
 
 
 }
