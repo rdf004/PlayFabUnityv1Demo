e6184eed40975cd5c791a502ba47ee487ab7aca2
rdf004 <32603533+rdf004@users.noreply.github.com>
2020-07-08 18:55:15 -0700

    Update StoreSetup.cs

--- StoreSetup.cs
+++ StoreSetup.cs
@@ -14,14 +14,13 @@ using UnityEngine.UI;
 
 public class StoreSetup : MonoBehaviour {
 
-    public static Button Login_Button, Arbitrary_Wishlist, Cat_Button, Dog_Button, Mango_Button;
+    /* Buttons that appear in our Unity game. */
 
-    void Start() {
-        Cat_Button = GameObject.FindGameObjectWithTag("Cat_Button").GetComponent<Button>();
-        Dog_Button = GameObject.FindGameObjectWithTag("Dog_Button").GetComponent<Button>();
-        Mango_Button = GameObject.FindGameObjectWithTag("Mango_Button").GetComponent<Button>();
+    public static Button Login_Button, Cat_Button, Dog_Button, Mango_Button;
 
-    }
+    /*
+        When the player logs in, set up the store in our Unity game. Namely, set the button colors and text.
+    */
 
     public static void StoreStart() {
         Cat_Button = GameObject.FindGameObjectWithTag("Cat_Button").GetComponent<Button>();
@@ -39,9 +38,27 @@ public class StoreSetup : MonoBehaviour {
 
     }
 
-    public static void SetUpStore(string item_ids, bool remove_item) {
+    /*
+        Set up the store buttons based on the CSV wish list OR update store after an item is removed from
+        the wish list.
 
-        // Ideally, this should go through and set text whether or not the button/item is on the list
+        @param item_ids: If initially setting up the store, CSV of ItemIDs on the wish list. If updating the store
+            after an ItemID is removed from the wish list, then the ItemID of the item that was removed.
+        @param remote_tem: true if an item is being removed, false otherwise.
+
+        The second parameter allows us to call this function when the store is initially set up, and also when
+        an item is removed from the wish list. 
+        
+        For initial setup, StoreSetup.StoreStart initializes all buttons to
+        the color white and text indicating that they are not on the wishlist. For all items on the wishlist, 
+        SetUpStore changes the corresponding button color to green and text to indicate that the item is on the wish list.
+
+        If an item is removed from the wishlist and we need to update the store, then the items array will have one element. That
+        one element is the ItemID for the item we removed. The function then changes the button color and text to indicate that
+        the item is no longer on the wish list.
+    */
+
+    public static void SetUpStore(string item_ids, bool remove_item) {
         
         string[] items = item_ids.Split(',');
 
